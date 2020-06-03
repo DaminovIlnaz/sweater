66b406fa31d1db65e4d0b2ee399d982061987f27
Ilnaz Daminov <IlnazDaminov@bk.ru>
2020-06-03 14:10:47 +0300

    start

---
+++ src/test/java/ru/itis/kpfu/sweater/SweaterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.kpfu.sweater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
